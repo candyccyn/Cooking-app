bf3bfedb1452820085f6fb3a934edb7e187e8a9a
bbestbt <oranichbest@gmail.com>
2021-02-18 21:15:13 +0700

    fix signin

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -24,76 +24,81 @@ class _SignInScreenState extends State<SignInScreen> {
             padding: EdgeInsets.symmetric(
               horizontal: 20,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Sign in",
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Color(0xFF012060),
-                    decoration: TextDecoration.none,
+            child: SingleChildScrollView(
+                          child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                  height:90,
+                ),
+                  Text(
+                    "Sign in",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Color(0xFF012060),
+                      decoration: TextDecoration.none,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 50.0,
-                ),
-                Text(
-                  "Sign in username and password \nor continue with social media",
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                SignForm(),
-                RoundedButton(
-                  text: "Login",
-                  press: () async {
-                    vmCooking.signInAnon();
-                    print(vmCooking.uid);
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Social(
-                      icon: "assets/icons/google.svg",
-                      press: () {}, // TODO implement auth service
-                    ),
-                    Social(
-                      icon: "assets/icons/facebook.svg",
-                      press: () {},
-                    ),
-                    Social(
-                      icon: "assets/icons/twitter.svg",
-                      press: () {},
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Don't have an account ? ",
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    GestureDetector(
-                        onTap: () =>
-                            Navigator.pushNamed(context, SignUpScreen.routeName),
-                        child: Text("Sign up ",
-                            style: TextStyle(
-                                fontSize: 15,
-                                color: Color(0xFFFFA925),
-                                fontWeight: FontWeight.bold))),
-                  ],
-                ),
-              ],
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  Text(
+                    "Sign in username and password \nor continue with social media",
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  SignForm(),
+                  RoundedButton(
+                    text: "Login",
+                    press: () async {
+                      vmCooking.signInAnon();
+                      print(vmCooking.uid);
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Social(
+                        icon: "assets/icons/google.svg",
+                        press: () {}, // TODO implement auth service
+                      ),
+                      Social(
+                        icon: "assets/icons/facebook.svg",
+                        press: () {},
+                      ),
+                      Social(
+                        icon: "assets/icons/twitter.svg",
+                        press: () {},
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account ? ",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      GestureDetector(
+                          onTap: () =>
+                              Navigator.pushNamed(context, SignUpScreen.routeName),
+                          child: Text("Sign up ",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: Color(0xFFFFA925),
+                                  fontWeight: FontWeight.bold))),
+                    ],
+                  ),
+                ],
+              ),
             ),
           )),
     );
