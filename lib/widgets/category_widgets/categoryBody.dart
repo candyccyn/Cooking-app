f403f899d559ad4dcbfab303c3e918dd8c975893
Ornny <61090037@kmitl.ac.th>
2021-04-14 21:04:17 +0700

    add menu in body

--- lib/widgets/category_widgets/categoryBody.dart
+++ lib/widgets/category_widgets/categoryBody.dart
@@ -1,9 +1,14 @@
+import 'package:cooking_app/view_models/menu_view_model.dart';
 import 'package:cooking_app/widgets/category_widgets/categoryHeader.dart';
+import 'package:cooking_app/widgets/menu_card.dart';
 import 'package:flutter/material.dart';
 
 class CategoryBody extends StatelessWidget {
+
+  MenuViewModel menuViewModel = MenuViewModel();
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
         body: Container(
           width: double.infinity,
@@ -26,6 +31,42 @@ class CategoryBody extends StatelessWidget {
                         borderRadius: BorderRadius.only(
                             topLeft: Radius.circular(40),
                             topRight: Radius.circular(40))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Asian Food',
+                            style: TextStyle(
+                                fontFamily: 'Century Gothic',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                                color: Color.fromRGBO(9, 29, 103, 1)),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(color: Colors.green),
+                              child: MediaQuery.removePadding(
+                                context: context,
+                                removeTop: true,
+                                child: GridView.count(
+                                  crossAxisCount: 2,
+                                  // crossAxisSpacing: 20,
+                                  // mainAxisSpacing: 20,
+                                  children: List.generate(
+                                      MenuCard.getMenuSize(), (index) {
+                                    return Container(
+                                      child: MenuCard(
+                                        image: menuViewModel.getImg(index),
+                                        title: menuViewModel.getText(index),
+                                      ),
+                                    );
+                                  }),
+                                ),
+                              )),
+                        ],
+                      ),
+                    ),
                   ),
                 );
               },
