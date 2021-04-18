285284bbf1100fc0cc65b7f3a4fac23a9cafab7b
Ornny <61090037@kmitl.ac.th>
2021-04-18 17:30:43 +0700

    edit comment bar

--- lib/widgets/recipeDetail_widgets/comment_bar.dart
+++ lib/widgets/recipeDetail_widgets/comment_bar.dart
@@ -24,32 +24,34 @@ class _CommentBarState extends State<CommentBar> {
                         image: new NetworkImage(
                             "https://www.dmarge.com/wp-content/uploads/2021/01/dwayne-the-rock-.jpg")))),
             SizedBox(width: 10),
-            Container(
-                width: 190,
-                height: 30,
-                padding: EdgeInsets.only(left: 10),
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(242, 141, 22, 0.35),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintStyle: TextStyle(
-                        fontFamily: 'Century Gothic',
-                        fontSize: 10,
-                        color: Color.fromRGBO(9, 29, 103, 1)),
-                    hintText: 'Comment here',
-                    suffixIcon: IconButton(
-                      onPressed: () => {},
-                      icon: Icon(
-                        Icons.send,
-                        color: Colors.white,
-                        size: 15,
-                      ),
-                    ),
-                    border: InputBorder.none,
+            Expanded(
+              child: Container(
+                // width: MediaQuery.of(context).size.width/2-30.0,
+              height: 30,
+                  padding: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(242, 141, 22, 0.35),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                )),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(
+                          fontFamily: 'Century Gothic',
+                          fontSize: 10,
+                          color: Color.fromRGBO(9, 29, 103, 1)),
+                      hintText: 'Comment here',
+                      suffixIcon: IconButton(
+                        onPressed: () => {},
+                        icon: Icon(
+                          Icons.send,
+                          color: Colors.white,
+                          size: 15,
+                        ),
+                      ),
+                      border: InputBorder.none,
+                    ),
+                  )),
+            ),
           ],
         ),
       ),
