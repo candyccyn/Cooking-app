922fcb6bd8985ab3b29446e60fb5ecf940dd9522
Ornny <ornnychaowai@gmail.com>
2021-04-27 01:10:10 +0700

    edit overflow welcome page

--- lib/models/welcome_body_data.dart
+++ lib/models/welcome_body_data.dart
@@ -1,7 +1,7 @@
 class WelcomeBodyData {
   List<Map<String, String>> data = [
-    {"text": "welcome", "image": "assets/images/cooking1.png"},
-    {"text": "to", "image": "assets/images/cooking2.png"},
+    {"text": "Let's have fun", "image": "assets/images/cooking1.png"},
+    {"text": "with", "image": "assets/images/cooking2.png"},
     {"text": "cooking", "image": "assets/images/cooking3.png"},
   ];
 }
